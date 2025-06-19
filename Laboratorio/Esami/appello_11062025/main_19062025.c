5efcac44baf8f85344ed70a39ca41091547a6758
francescounitn <francesco.dallagata@studenti.unitn.it>
2025-06-19 16:42:55 +0200

    reorganized the entire directory tree to make space for the other part of the exam 'teoria', added some notes, started another attempt of appello 11062025

---
+++ Laboratorio/Esami/appello_11062025/main_19062025.c
@@ -0,0 +1,29 @@
+// main_19062025.c
+
+// Section 0: author & introduction
+// Author: Francesco Dall'Agata
+// Introduction: exam of 11/06/2025 of Sistemi Operativi (Laboratorio) by PhD M. Grisafi
+
+// Section 1: index
+// 0) index
+// 1) author & introduction
+// 2) include
+// 3) define
+// 4) typedef
+// 5) global variables
+// 6) function declaration
+// 7) main
+// 8) function definition
+
+// Section 2: include
+#include <stdlib.h>
+#include <stdio.h>
+
+//Section 3: define
+#define STR_DEFAULT 128
+
+//Section 4: typedef
+
+//Section 5: global variables
+
+//Section 6: function declaration
